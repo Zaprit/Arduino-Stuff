43b9373ea9cddadefe01a8b79aedb20f78d0e315
Zaprit <henry@vorax.org>
2017-04-18 18:30:47 +0100

    Update for the bomb game

---
+++ BombGame/BombGame.ino/BombGame.ino.ino
@@ -0,0 +1,36 @@
+const int bottom_left = 0;
+const int bottom = 1;
+const int bottom_right = 2;
+
+const int top_left = 6;
+const int top = 7;
+const int top_right = 5;
+
+const int middle = 4;
+const int dot = 3;
+
+void setup() {
+
+  // put your setup code here, to run once:
+  pinMode(bottom_left, output);
+  pinMode(bottom, output);
+  pinMode(bottom_right, output);
+
+  pinMode(top_left, output);
+  pinMode(top, output);
+  pinMode(top_right, output);
+
+  pinMode(middle, output);
+  pinMode(dot, output);
+  
+}
+
+void printLED(String s){
+if(s == "")
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
